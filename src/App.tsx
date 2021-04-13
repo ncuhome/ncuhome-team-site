907c09ff3f4987a50ae66aa22c6648f5543e67ec
ccarmellia <1349074040@qq.com>
2021-04-14 00:49:55 +0800

    refactor:change home-header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HomeHeader from "./pages/HomeHeader";
+import HomeHeader from "./pages/home/Ncuhome/HomeHeader";
 import Routes from "./routers";
 
 const App: React.FC = () => {
