c1e16772e5fbf2fa1de45c50f4875b13580c7941
ccarmellia <1349074040@qq.com>
2021-04-15 01:34:05 +0800

    chore: modify blog-list

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -2,6 +2,8 @@ import About from "@/pages/detail/About";
 import Blog from "@/pages/detail/Blog";
 import Product from "@/pages/detail/Product";
 import Team from "@/pages/detail/Team";
+import NewRegister from "@/pages/detail/Register/NewRegister"
+import OldRegister from "@/pages/detail/Register/OldRegister"
 import React from "react";
 import { BrowserRouter, Switch, Route} from "react-router-dom";
 import Home from "../pages/home";
@@ -15,7 +17,8 @@ const Routes: React.FC = () => {
         <Route path="/team" component={Team} />
         <Route path="/blog" component={Blog} />
         <Route path="/about" component={About} />
-        <Route path="/register" component={About} />
+        <Route path="/newregister" component={NewRegister} />
+        <Route path="/oldregister" component={OldRegister} />
       </Switch>
     </BrowserRouter>
   );
