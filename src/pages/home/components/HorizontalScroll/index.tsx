4b0d3e986dfea43119075f51c8f5376dc7837dbc
Jungbey <1952325619@qq.com>
2021-07-27 14:56:23 +0800

    :sparkles: feat: 优化横向滚动组件

--- src/pages/home/components/HorizontalScroll/index.tsx
+++ src/pages/home/components/HorizontalScroll/index.tsx
@@ -1,26 +1,50 @@
-import React, { useEffect, useState } from "react";
-import { useWindowScroll } from "react-use";
+import React, { createRef } from "react";
 import './style.scss';
 
-const HorizontalScroll: React.FC = ({ children }) => {
-  const { y } = useWindowScroll();
-  const [computedOffset, setComputedOffset] = useState(0);
+let resizeTimeout: NodeJS.Timeout | null;
 
-  useEffect(() => {
-    const width = 4950
-    if (y <= width) {
-      setComputedOffset(y);
+const throttle = (
+  e: React.WheelEvent<HTMLDivElement>,
+  fb: React.WheelEventHandler<HTMLDivElement>
+) => {
+  if (!resizeTimeout) {
+    resizeTimeout = setTimeout(() => {
+      resizeTimeout = null;
+      fb(e);
+    }, 20);
+  }
+};
+
+const HorizontalScroll: React.FC = ({ children }) => {
+  const hsContent = createRef<HTMLDivElement>();
+
+  const handleWheel: React.WheelEventHandler<HTMLDivElement> = (e) => {
+    console.log("wheel");
+    const { deltaY } = e;
+    const { scrollWidth, offsetWidth, scrollLeft } = hsContent.current;
+    const maxScroll = scrollWidth - offsetWidth;
+
+    //TODO: 修改滚动方案，让让滚动更丝滑
+    if (deltaY > 0) {
+      /* 向下滚动 */
+      if (scrollLeft < maxScroll) {
+        hsContent.current.scrollLeft += 100;
+      }
+    } else {
+      /* 向上滚动 */
+      if (scrollLeft > 0) {
+        hsContent.current.scrollLeft -= 100;
+      }
     }
-    if (y > width) {
-    }
-  }, [y]);
+  }
 
   return (
     <div className="hs-body">
       <div className="hs-content-limit" style={{ position: "fixed" }}>
         <div
           className="hs-horizontal-content"
-          style={{ transform: `translateX(-${computedOffset}px)` }}
+          ref={hsContent}
+          onWheel={e => throttle(e, handleWheel)}
         >
           {children}
         </div>
