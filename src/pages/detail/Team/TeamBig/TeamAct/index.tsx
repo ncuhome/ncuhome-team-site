bf87209d560bcc081cd4da163bec5a8534aa8a25
UniversessH <universess@qq.com>
2021-09-07 00:17:43 +0800

    feat: 产品页增加气泡浮动效果

---
+++ src/pages/detail/Team/TeamBig/TeamAct/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import teamActTrail from "@/assets/img/team-act-trail.svg";
+import "./style.scss"
+
+
+interface childProps {
+  imgName: string,
+  ActTitle_EN: string,
+  ActTitle_CN: string,
+  ActTime: string,
+  ActDes: string,
+}
+
+const TeamAct: React.FC<childProps> = (props) => {
+  console.log(props.imgName)
+  return (
+    <div className="team-act-wrapper">
+      <img src={props.imgName} className="team-act-img" />
+      <img src={teamActTrail} className="team-act-trail" />
+      <div className="team-act-text">
+        <div>
+          <h1 style={{fontSize:"30px"}}>{props.ActTitle_EN}</h1><br />
+          <span style={{fontSize:"30px"}}>{props.ActTitle_CN} </span>
+          <span style={{fontSize:"15px",marginLeft:"8px"}}>{props.ActTime}</span>
+        </div>
+        <div style={{fontSize:"17px",marginTop:"20px",whiteSpace:"pre"}}>
+          {props.ActDes}
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default TeamAct;
