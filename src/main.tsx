3588b8f640416f204a1e8fde1b7b0dbb4e47aaaa
Kuss <2360314753@qq.com>
2021-09-08 22:19:14 +0800

    fix: App should not be wrapped by BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom'
 import 'reset.css/reset.css';
 import './index.css'
 import App from './App'
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <App />,
   document.getElementById('root')
 )
