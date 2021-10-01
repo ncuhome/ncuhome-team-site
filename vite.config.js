5dba3328f1d4c2d343d167d5a944916a0593c35d
FuzzyFade <1339184537@qq.com>
2021-10-01 18:12:18 +0800

    fix: vite config error

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
-import { resolve } from "path";
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
