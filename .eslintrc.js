464ca31e7a56a3ad8e08a6df9ac06707a2a2c3af
Jungbey <1952325619@qq.com>
2021-10-04 20:34:17 +0800

    :wrench: feat: config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+  },
   extends: [
     'plugin:react/recommended',
     'airbnb',
@@ -10,17 +18,38 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
       tsx: true,
     },
     ecmaVersion: 12,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: [
     'react',
     '@typescript-eslint',
   ],
   rules: {
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/jsx-filename-extension': [
+      2,
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+        ]
+      }
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        'js': 'never',
+        'jsx': 'never',
+        'ts': 'never',
+        'tsx': 'never'
+      },
+    ],
+    'no-use-before-define': 'off'
   },
 };
